56e8a758bd701ca87062c76732e928b3e06d5682
Donn8 <98732618+Donn8@users.noreply.github.com>
2023-08-25 10:44:10 +0800

    home.php

--- Embedded_Finals/home.php
+++ Embedded_Finals/home.php
@@ -19,7 +19,7 @@
       .packet {color: #bebebe;}
       .temperatureColor {color: #fd7e14;}
       .humidityColor {color: #1b78e2;}
-	  .soilmoistureColor {color: #8a4603;}
+	    .soilmoistureColor {color: #8a4603;}
       .statusreadColor {color: #702963; font-size:12px;}
       .LEDColor {color: #183153;}
       
@@ -118,15 +118,15 @@
           
           <!-- Displays the humidity and temperature values received from ESP32. *** -->
           <h4 class="temperatureColor"><i class="fas fa-thermometer-half"></i> TEMPERATURE</h4>
-          <p class="temperatureColor"><span class="reading"><span id="ESP32_01_Temp"></span> &deg;C</span></p>
+          <p class="temperatureColor"><span class="reading"><span id="ESP8266_01_Temp"></span> &deg;C</span></p>
           <h4 class="humidityColor"><i class="fas fa-tint"></i> HUMIDITY</h4>
-          <p class="humidityColor"><span class="reading"><span id="ESP32_01_Humd"></span> &percnt;</span></p>
-		  <h4 class="soilmoistureColor"><i class="fas fa-tint"></i> SOIL MOISTURE</h4>
-          <p class="soilmoistureColor"><span class="reading"><span id="ESP32_01_SoilM"></span> &percnt;</span></p>
+          <p class="humidityColor"><span class="reading"><span id="ESP8266_01_Humd"></span> &percnt;</span></p>
+		      <h4 class="soilmoistureColor"><i class="fas fa-tint"></i> SOIL MOISTURE</h4>
+          <p class="soilmoistureColor"><span class="reading"><span id="ESP8266_01_SoilM"></span> &percnt;</span></p>
 		  
           <!-- *********************************************************************** -->
           
-          <p class="statusreadColor"><span>Status Read Sensor DHT22 : </span><span id="ESP32_01_Status_Read_DHT11"></span></p>
+          <p class="statusreadColor"><span>Status Read Sensor DHT22 : </span><span id="ESP8266_01_Status_Read_DHT22"></span></p>
         </div>
         <!-- ======================================================================================================= -->
         
@@ -139,12 +139,12 @@
           <!-- Buttons for controlling the LEDs on Slave 2. ************************** -->
           <h4 class="LEDColor"><i class="fas fa-lightbulb"></i> LED 1</h4>
           <label class="switch">
-            <input type="checkbox" id="ESP32_01_TogLED_01" onclick="GetTogBtnLEDState('ESP32_01_TogLED_01')">
+            <input type="checkbox" id="ESP8266_01_TogLED_01" onclick="GetTogBtnLEDState('ESP8266_01_TogLED_01')">
             <div class="sliderTS"></div>
           </label>
           <h4 class="LEDColor"><i class="fas fa-lightbulb"></i> LED 2</h4>
           <label class="switch">
-            <input type="checkbox" id="ESP32_01_TogLED_02" onclick="GetTogBtnLEDState('ESP32_01_TogLED_02')">
+            <input type="checkbox" id="ESP8266_01_TogLED_02" onclick="GetTogBtnLEDState('ESP8266_01_TogLED_02')">
             <div class="sliderTS"></div>
           </label>
           <!-- *********************************************************************** -->
@@ -159,7 +159,7 @@
     <div class="content">
       <div class="cards">
         <div class="card header" style="border-radius: 15px;">
-            <h3 style="font-size: 0.7rem;">LAST TIME RECEIVED DATA FROM ESP8266 [ <span id="ESP32_01_LTRD"></span> ]</h3>
+            <h3 style="font-size: 0.7rem;">LAST TIME RECEIVED DATA FROM ESP8266 [ <span id="ESP8266_01_LTRD"></span> ]</h3>
             <button onclick="window.open('recordtable.php', '_blank');">Open Record Table</button>
             <h3 style="font-size: 0.7rem;"></h3>
         </div>
@@ -169,11 +169,11 @@
     
     <script>
       //------------------------------------------------------------
-      document.getElementById("ESP32_01_Temp").innerHTML = "NN"; 
-      document.getElementById("ESP32_01_Humd").innerHTML = "NN";
-	  document.getElementById("ESP32_01_SoilM").innerHTML = "NN";
-      document.getElementById("ESP32_01_Status_Read_DHT11").innerHTML = "NN";
-      document.getElementById("ESP32_01_LTRD").innerHTML = "NN";
+      document.getElementById("ESP8266_01_Temp").innerHTML = "NN"; 
+      document.getElementById("ESP8266_01_Humd").innerHTML = "NN";
+	    document.getElementById("ESP8266_01_SoilM").innerHTML = "NN";
+      document.getElementById("ESP8266_01_Status_Read_DHT22").innerHTML = "NN";
+      document.getElementById("ESP8266_01_LTRD").innerHTML = "NN";
       //------------------------------------------------------------
       
       Get_Data("esp8266_01");
@@ -199,20 +199,20 @@
           if (this.readyState == 4 && this.status == 200) {
             const myObj = JSON.parse(this.responseText);
             if (myObj.id == "esp8266_01") {
-              document.getElementById("ESP32_01_Temp").innerHTML = myObj.temperature;
-              document.getElementById("ESP32_01_Humd").innerHTML = myObj.humidity;
-			  document.getElementById("ESP32_01_SoilM").innerHTML = myObj.soilmoisture;
-              document.getElementById("ESP32_01_Status_Read_DHT11").innerHTML = myObj.status_read_sensor_dht22;
-              document.getElementById("ESP32_01_LTRD").innerHTML = "Time : " + myObj.ls_time + " | Date : " + myObj.ls_date + " (dd-mm-yyyy)";
+              document.getElementById("ESP8266_01_Temp").innerHTML = myObj.temperature;
+              document.getElementById("ESP8266_01_Humd").innerHTML = myObj.humidity;
+			        document.getElementById("ESP8266_01_SoilM").innerHTML = myObj.soilmoisture;
+              document.getElementById("ESP8266_01_Status_Read_DHT22").innerHTML = myObj.status_read_sensor_dht22;
+              document.getElementById("ESP8266_01_LTRD").innerHTML = "Time : " + myObj.ls_time + " | Date : " + myObj.ls_date + " (dd-mm-yyyy)";
               if (myObj.LED_01 == "ON") {
-                document.getElementById("ESP32_01_TogLED_01").checked = true;
+                document.getElementById("ESP8266_01_TogLED_01").checked = true;
               } else if (myObj.LED_01 == "OFF") {
-                document.getElementById("ESP32_01_TogLED_01").checked = false;
+                document.getElementById("ESP8266_01_TogLED_01").checked = false;
               }
               if (myObj.LED_02 == "ON") {
-                document.getElementById("ESP32_01_TogLED_02").checked = true;
+                document.getElementById("ESP8266_01_TogLED_02").checked = true;
               } else if (myObj.LED_02 == "OFF") {
-                document.getElementById("ESP32_01_TogLED_02").checked = false;
+                document.getElementById("ESP8266_01_TogLED_02").checked = false;
               }
             }
           }
@@ -225,14 +225,14 @@
       
       //------------------------------------------------------------
       function GetTogBtnLEDState(togbtnid) {
-        if (togbtnid == "ESP32_01_TogLED_01") {
+        if (togbtnid == "ESP8266_01_TogLED_01") {
           var togbtnchecked = document.getElementById(togbtnid).checked;
           var togbtncheckedsend = "";
           if (togbtnchecked == true) togbtncheckedsend = "ON";
           if (togbtnchecked == false) togbtncheckedsend = "OFF";
           Update_LEDs("esp8266_01","LED_01",togbtncheckedsend);
         }
-        if (togbtnid == "ESP32_01_TogLED_02") {
+        if (togbtnid == "ESP8266_01_TogLED_02") {
           var togbtnchecked = document.getElementById(togbtnid).checked;
           var togbtncheckedsend = "";
           if (togbtnchecked == true) togbtncheckedsend = "ON";
